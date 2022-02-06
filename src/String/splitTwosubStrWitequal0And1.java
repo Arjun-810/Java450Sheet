c4552787d318ff3f5209cd9cd1ae9e32716f751a
arjun kaushik <arjunkaushik810@gmail.com>
2022-02-06 21:05:57 +0530

    waste day

--- src/String/splitTwosubStrWitequal0And1.java
+++ src/String/splitTwosubStrWitequal0And1.java
@@ -6,9 +6,9 @@ public class splitTwosubStrWitequal0And1 {
     public static String[] splitString(String str) {
         int zeros = 0,ones = 0;
         for (int i=0;i<str.length();i++){
-            if(str.charAt(i) == 1)
+            if(str.charAt(i) == '1')
                 zeros++;
-            else
+            else if (str.charAt(i) == '0')
                 ones++;
         }
         int str1_zeroes = 0;
@@ -18,22 +18,7 @@ public class splitTwosubStrWitequal0And1 {
         int x = 0,y = 0;
         String str1 = "",str2 = "";
         for (int i=0;i<str.length();i++){
-            if (str1_zeroes <= zeros/2 && str1_ones <= ones/2) {
-                if (str.charAt(i) == 0)
-                    str1_zeroes++;
-                if (str.charAt(i) == 1)
-                    str1_ones++;
-                x++;
-                str1 += str.charAt(i);
-            }
-            if (str2_zeroes <= zeros/2 && str2_ones <= ones/2) {
-                if (str.charAt(str.length() - i - 1) == 0)
-                    str2_zeroes++;
-                if (str.charAt(str.length() - i - 1) == 1)
-                    str2_ones++;
-                y++;
-                str2 += str.charAt(i);
-            }
+            
         }
         System.out.println(str1);
         System.out.println(str2);
